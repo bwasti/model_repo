d519935e7a1757b5a5ef5b473b2e44cb4bbb5fa2
bwasti <bwasti@fb.com>
2020-06-04 17:08:44 +0000

    add dataset

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ python -m pip install torch
 python -m pip install 'git+https://github.com/facebookresearch/detectron2.git'
 python -m pip install Cython
 python -m pip install pycocotools 
+python -m pip install tensorboard
 pushd TensorMask
 python setup.py build develop
 popd
--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python TensorMask/train_net.py --config=TensorMask/configs/Base-TensorMask.yaml
+python TensorMask/train_net.py --config=TensorMask/configs/tensormask_R_50_FPN_6x.yaml --eval-only
